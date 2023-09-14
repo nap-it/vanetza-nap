821eb413bb59d30d341094a737acf27789d86d92
Rodrigo <r.rosmaninho@ua.pt>
2023-09-14 12:14:10 +0100

    Update local config from board config on container initialization

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
+update_config_field() {
+    if grep -qE "^\[$2\]" "$1" && grep -qE "^$3\s*=" "$1"; then
+        val=$(grep -E "^\[$2\]" -A 1000 "$1" | grep -m 1 -E "^$3\s*=" | sed -E 's/^[^=]*=\s*//')
+        sed "/^\[$5]/,/^\[/{s/^$6[[:space:]]*=.*/$6 = $val/}" "$4" > "$4.tmp" && cp "$4.tmp" "$4" && rm "$4.tmp";
+    else
+        echo "Field '$3' in section '$2' not found in '$1'. Skipping update."
+    fi
+}
+
+if [ -e "/info.ini" ]; then
+    update_config_field "/info.ini" "general" "id" "/config.ini" "station" "id"
+    update_config_field "/info.ini" "mobility" "stationType" "/config.ini" "station" "type"
+    update_config_field "/info.ini" "mobility" "latitude" "/config.ini" "station" "latitude"
+    update_config_field "/info.ini" "mobility" "longitude" "/config.ini" "station" "longitude"
+    update_config_field "/info.ini" "mobility" "macAddr" "/config.ini" "station" "mac_address"
+    update_config_field "/info.ini" "mobility" "interface" "/config.ini" "general" "interface"
+    echo "Config update process complete"
+else
+    echo "No global board config file found. Skipping config update process"
+fi
+
 IP_ADDR=$(ip -f inet addr show eth0 | awk '/inet / {print $2}')
 GW_ADDR=$(ip r | awk '/default / {print $3}')
 BR_ID=br0
