e375020d3fa86d76c3d89f1c640848663847ed1b
jp.amaral <jp.amaral@av.it.pt>
2025-06-30 15:46:38 +0100

    updated dds configs to use xml without useBuiltinTransports

--- tools/socktap/fastdds/DDSCommon.hpp
+++ tools/socktap/fastdds/DDSCommon.hpp
@@ -5,7 +5,8 @@
 #include <fastdds/dds/domain/DomainParticipant.hpp>
 #include <fastdds/dds/publisher/Publisher.hpp>
 #include <fastdds/dds/subscriber/Subscriber.hpp>
-#include <fastdds/rtps/transport/UDPv4TransportDescriptor.h>
+// #include <fastdds/rtps/transport/UDPv4TransportDescriptor.h>
+// #include <fastdds/rtps/transport/shared_mem/SharedMemTransportDescriptor.h>
 
 using namespace eprosima::fastdds::dds;
 
@@ -18,11 +19,54 @@ inline DomainParticipant* get_participant(const eprosima::fastrtps::fixed_string
     if (participant_ == nullptr) {
         DomainParticipantQos participantQos;
         participantQos.name(participantName);
-        auto udp_transport = std::make_shared<eprosima::fastdds::rtps::UDPv4TransportDescriptor>();
-        udp_transport->interface_blocklist.emplace_back("wlan0");
-        participantQos.transport().user_transports.push_back(udp_transport);
-        participantQos.transport().use_builtin_transports = false;
-        participant_ = DomainParticipantFactory::get_instance()->create_participant(domain_id, participantQos);
+        // auto udp_transport = std::make_shared<eprosima::fastdds::rtps::UDPv4TransportDescriptor>();
+        // udp_transport->interface_blocklist.emplace_back("wlan0");
+        // participantQos.transport().user_transports.push_back(udp_transport);
+        // auto shm_transport = std::make_shared<eprosima::fastdds::rtps::SharedMemTransportDescriptor>();
+        // shm_transport->interface_blocklist.emplace_back("wlan0");
+        // participantQos.transport().user_transports.push_back(shm_transport);
+        // participantQos.transport().use_builtin_transports = true;
+        // participant_ = DomainParticipantFactory::get_instance()->create_participant(domain_id, participantQos);
+
+        
+        std::string xml_profile =
+        "\
+        <?xml version=\"1.0\" encoding=\"UTF-8\" ?>\
+            <profiles xmlns=\"http://www.eprosima.com/XMLSchemas/fastRTPS_Profiles\">\
+                <transport_descriptors>\
+                    <transport_descriptor>\
+                        <transport_id>CustomUdpTransportBlocklist</transport_id>\
+                        <type>UDPv4</type>\
+                        <interfaces>\
+                            <blocklist>\
+                                <interface name=\"wlan0\"/>\
+                            </blocklist>\
+                        </interfaces>\
+                    </transport_descriptor>\
+                    <transport_descriptor>\
+                        <transport_id>CustomSHMTransportBlocklist</transport_id>\
+                        <type>SHM</type>\
+                        <interfaces>\
+                            <blocklist>\
+                                <interface name=\"wlan0\"/>\
+                            </blocklist>\
+                        </interfaces>\
+                    </transport_descriptor>\
+                </transport_descriptors>\
+                <participant profile_name=\"CustomTcpTransportBlocklistParticipant\">\
+                    <rtps>\
+                        <name>Vanetza</name>\
+                        <userTransports>\
+                            <transport_id>CustomUdpTransportBlocklist</transport_id>\
+                            <transport_id>CustomSHMTransportBlocklist</transport_id>\
+                        </userTransports>\
+                        <useBuiltinTransports>false</useBuiltinTransports>\
+                    </rtps>\
+                </participant>\
+            </profiles>\
+        ";
+        DomainParticipantFactory::get_instance()->load_XML_profiles_string(xml_profile.c_str(),xml_profile.length());
+        participant_ = DomainParticipantFactory::get_instance()->create_participant_with_profile(domain_id, "CustomTcpTransportBlocklistParticipant");
 
         if (participant_ == nullptr) {
             std::cout << "[Participant] Not created" << std::endl;
