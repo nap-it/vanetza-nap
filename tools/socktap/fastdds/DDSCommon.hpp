5b2d3e3dc0f8ac557c1134284ffb95ab3b9ee68e
jp.amaral <jp.amaral@av.it.pt>
2025-08-19 16:18:52 +0100

    Restored wlan0 blocking temporarily

--- tools/socktap/fastdds/DDSCommon.hpp
+++ tools/socktap/fastdds/DDSCommon.hpp
@@ -18,61 +18,11 @@ inline DomainParticipant* get_participant(const eprosima::fastrtps::fixed_string
     if (participant_ == nullptr) {
         DomainParticipantQos participantQos;
         participantQos.name(participantName);
-        // auto udp_transport = std::make_shared<eprosima::fastdds::rtps::UDPv4TransportDescriptor>();
-        // udp_transport->interface_blocklist.emplace_back("wlan0");
-        // participantQos.transport().user_transports.push_back(udp_transport);
-        // participantQos.transport().use_builtin_transports = false;
+        auto udp_transport = std::make_shared<eprosima::fastdds::rtps::UDPv4TransportDescriptor>();
+        udp_transport->interface_blocklist.emplace_back("wlan0");
+        participantQos.transport().user_transports.push_back(udp_transport);
+        participantQos.transport().use_builtin_transports = false;
         participant_ = DomainParticipantFactory::get_instance()->create_participant(domain_id, participantQos);
-
-        
-        // std::string xml_profile =
-        // "\
-        // <?xml version=\"1.0\" encoding=\"UTF-8\" ?>\
-        //     <profiles xmlns=\"http://www.eprosima.com/XMLSchemas/fastRTPS_Profiles\">\
-        //         <transport_descriptors>\
-        //             <transport_descriptor>\
-        //                 <transport_id>CustomUdpTransportBlocklist</transport_id>\
-        //                 <type>UDPv4</type>\
-        //                 <interfaces>\
-        //                     <blocklist>\
-        //                         <interface name=\"wlan0\"/>\
-        //                     </blocklist>\
-        //                 </interfaces>\
-        //             </transport_descriptor>\
-        //             <transport_descriptor>\
-        //                 <transport_id>CustomTcpTransportBlocklist</transport_id>\
-        //                 <type>TCPv4</type>\
-        //                 <interfaces>\
-        //                     <blocklist>\
-        //                         <interface name=\"wlan0\"/>\
-        //                     </blocklist>\
-        //                 </interfaces>\
-        //             </transport_descriptor>\
-        //             <transport_descriptor>\
-        //                 <transport_id>CustomSHMTransportBlocklist</transport_id>\
-        //                 <type>SHM</type>\
-        //                 <interfaces>\
-        //                     <blocklist>\
-        //                         <interface name=\"wlan0\"/>\
-        //                     </blocklist>\
-        //                 </interfaces>\
-        //             </transport_descriptor>\
-        //         </transport_descriptors>\
-        //         <participant profile_name=\"CustomTransportBlocklistParticipant\">\
-        //             <rtps>\
-        //                 <name>Vanetza</name>\
-        //                 <userTransports>\
-        //                     <transport_id>CustomUdpTransportBlocklist</transport_id>\
-        //                     <transport_id>CustomSHMTransportBlocklist</transport_id>\
-        //                     <transport_id>CustomTcpTransportBlocklist</transport_id>\
-        //                 </userTransports>\
-        //                 <useBuiltinTransports>false</useBuiltinTransports>\
-        //             </rtps>\
-        //         </participant>\
-        //     </profiles>\
-        // ";
-        // DomainParticipantFactory::get_instance()->load_XML_profiles_string(xml_profile.c_str(),xml_profile.length());
-        // participant_ = DomainParticipantFactory::get_instance()->create_participant_with_profile(domain_id, "CustomTransportBlocklistParticipant");
 
         if (participant_ == nullptr) {
             std::cout << "[Participant] Not created" << std::endl;
